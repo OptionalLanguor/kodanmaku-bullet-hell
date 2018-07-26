8eaa037deb72e52ba668db21fa5c642c694ab1bd
Languor <felmt@hotmail.com>
2018-07-26 02:27:52 +0000

    Build and linking fix.

--- src/Inimigo.cpp
+++ src/Inimigo.cpp
@@ -1,5 +1,5 @@
-// Jogo Asteroids 
-// triangle.cpp
+// Jogo Asteroids
+// Inimigo.cpp
 // Prof. Giovani Bernardes Vitor
 // ECO034 - 2017
 
@@ -21,7 +21,7 @@ Inimigo::Inimigo() //Funciona sem "Player::"??
 	points = new double*[3];
 	for(int i=0; i<3; i++)
 		points[i] = new double[3];
-	
+
 	//-2.0, -1.0
 	points[0][0] = -2;
 	points[1][0] = -1;
@@ -42,13 +42,13 @@ Inimigo::Inimigo() //Funciona sem "Player::"??
 
 	points_base[0] = glm::vec3( 15.0f,   15.0f , 1.0f);
         points_base[1] = glm::vec3( 15.0f,  30.0f , 1.0f);
-        points_base[2] = glm::vec3( 30.0f,  30.0f , 1.0f) ;      
-	points_base[3] = glm::vec3( 30.0f,  15.0f , 1.0f) ; 
+        points_base[2] = glm::vec3( 30.0f,  30.0f , 1.0f) ;
+	points_base[3] = glm::vec3( 30.0f,  15.0f , 1.0f) ;
 
 	points[0] = points_base[0];
 	points[1] = points_base[1];
 	points[2] = points_base[2];
-	points_base[3] = points_base[3] ; 
+	points_base[3] = points_base[3] ;
 
 //	std::cout << "inicio: " << glm::to_string(points[0]);
 	//points = *point_;
@@ -80,12 +80,12 @@ void Inimigo::transform(void)
 	//glm::vec4 Position = glm::vec4(glm::vec3(0.0), 1.0);
         glm::mat3 Model = glm::mat3(1.0);
         Model[2] = glm::vec3(x, y, 1.0);
-	
+
 
 	//std::cout << "tes: " << glm::to_string(Model[0]) << std::endl;
 	//std::cout << "tes: " << glm::to_string(Model[1])<< std::endl;
 	//std::cout << "tes: " << glm::to_string(Model[2])<< "\n" << std::endl;
-	
+
         //glm::vec4 Transformed = Model * Position;
 
 
@@ -105,7 +105,7 @@ void Inimigo::draw()
 
 	//transform();
 	//std::cout << "tes: " << glm::to_string(points[0]);
-        glBegin(GL_SQUAD);  
+        glBegin(GL_QUADS);
 			glVertex2f(points[0].x,points[0].y);
 			glVertex2f(points[1].x,points[1].y);
 			glVertex2f(points[2].x,points[2].y);
